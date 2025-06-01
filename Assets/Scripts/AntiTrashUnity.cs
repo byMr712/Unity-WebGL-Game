9fb758f7e84092fe072c7633a87f82055e3041df
byMr712 <dzgrok@gmail.com>
2025-06-01 17:57:03 +0800

    button animation fix & new tutorial added

---
+++ Assets/Scripts/AntiTrashUnity.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class AntiTrashUnity : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    public GameObject[] Escape_to_menu;
+    void Start()
+    {
+       
+    }
+}
